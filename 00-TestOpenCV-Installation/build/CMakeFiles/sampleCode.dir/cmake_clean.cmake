267d70076ffbfbee2e59406b7ad231609fe6076f
vlad0x <vladcuevas@gmail.com>
2021-02-10 10:12:41 -0500

    Test OPENCV Installation

---
+++ 00-TestOpenCV-Installation/build/CMakeFiles/sampleCode.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/sampleCode.dir/sampleCode.cpp.o"
+  "sampleCode"
+  "sampleCode.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/sampleCode.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
