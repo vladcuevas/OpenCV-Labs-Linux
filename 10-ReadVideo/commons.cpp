f690aed10965ea8fc60ed86659dc373787fbc93b
vlad0x <vladcuevas@gmail.com>
2021-02-10 13:43:52 -0500

    Read Video

---
+++ 10-ReadVideo/commons.cpp
@@ -0,0 +1,31 @@
+#include <unistd.h>
+#include "commons.h"
+
+#define GetCurrentDir getcwd
+
+using namespace std;
+
+std::string commons::GetCurrentWorkingDir(void)
+{
+  char buff[FILENAME_MAX];
+  getcwd(buff, FILENAME_MAX);
+  std::string current_working_dir(buff);
+  return current_working_dir;
+}
+
+std::string commons::GetImageDataPath(string path)
+{
+  commons c;
+  string DATA_PATH = c.GetCurrentWorkingDir();
+
+  //Create new Images
+
+  //cout << DATA_PATH << endl;
+
+  string imgPath = DATA_PATH+path;
+
+  //cout << imgPath << endl;
+
+  return imgPath;
+};
+
---
+++ 10-ReadVideo/video_cv.cpp
@@ -0,0 +1,57 @@
+// Include libraries
+#include <opencv2/opencv.hpp>
+#include <opencv2/core.hpp>
+#include <opencv2/highgui.hpp>
+#include <iostream> // std::cout
+#include <string>   // std::string, std::to_string
+
+#include <stdio.h> /* defines FILENAME_MAX */
+// #define WINDOWS  /* uncomment this line to use it for windows.*/
+
+#include <unistd.h>
+
+#include "video_cv.h"
+#define GetCurrentDir getcwd
+
+using namespace cv;
+using namespace std;
+
+VideoCapture video_cv::displayVideoFromPath(string videoPath, bool isDisplay, int delay)
+{
+  VideoCapture cap(videoPath);
+
+  // Check if camera opened successfully
+  if(!cap.isOpened()){
+      cout << "Error opening video stream or file" << endl;
+  }
+
+  if (isDisplay == true) {
+
+    // Read until video is completed
+    while(cap.isOpened()){
+
+        Mat frame;
+
+        // Capture frame-by-frame
+        cap >> frame;
+
+        // If the frame is empty, break immediately
+        if (frame.empty())
+          break;
+
+        // Write the frame into the file 'outputChaplin.mp4'
+        imshow("Frame", frame);
+
+        // Wait for 25 ms before moving on to the next frame
+        // This will slow down the video 
+        waitKey(25);
+    }
+
+    destroyAllWindows();
+
+  }
+
+  VideoCapture cap2(videoPath);
+
+  return cap2;
+}
